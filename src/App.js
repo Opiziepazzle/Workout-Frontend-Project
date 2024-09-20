2e94e371fb0621b789eae879edea3b784bbe88c2
Adigun Opeyemi <opizie47@gmail.com>
2024-09-19 22:49:40 -0700

    implementation

--- src/App.js
+++ src/App.js
@@ -1,38 +1,3 @@
-// import React from "react";
-// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import { useAuthContext } from "./Hooks/UseAuthContext";
-// import Home from "./Pages/home";
-// import Nav from "./components/Navbar";
-// import Login from "./Pages/Login";
-// import SignUp from "./Pages/SignUp";
-
-
-
-
-// function App() {
-//   const { user } = useAuthContext();
-
-//   return (
-//     <div className="App">
-//       <BrowserRouter>
-//         <Nav />
-//         <div className="max-w-screen-2xl mx-auto">
-//           <Routes>
-//             <Route path="/" element={user ? <Home /> : <Navigate to={'/login'} /> } />
-//             <Route path="/login" element={!user ? <Login /> : <Navigate to={'/'} /> } />
-//             <Route path="/signup" element={!user ? <SignUp /> : <Navigate to={'/'} /> } />
-//           </Routes>
-//         </div>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
 
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
--- src/components/WorkoutsList.jsx
+++ src/components/WorkoutsList.jsx
@@ -1,34 +1,3 @@
-// import { createContext, useReducer } from 'react';
-
-// // Create the context
-// export const WorkoutsContext = createContext();
-
-// // Reducer to manage the workout state actions
-// const workoutsReducer = (state, action) => {
-//   switch (action.type) {
-//     case 'SET_WORKOUTS':
-//       return { workouts: action.payload };
-//     case 'CREATE_WORKOUT':
-//       return { workouts: [action.payload, ...state.workouts] };
-//     case 'DELETE_WORKOUT':
-//       return { workouts: state.workouts.filter((w) => w._id !== action.payload._id) };
-//     default:
-//       return state;
-//   }
-// };
-
-// // WorkoutsContextProvider that wraps the application to provide the workouts state
-// export const WorkoutsContextProvider = ({ children }) => {
-//   const [state, dispatch] = useReducer(workoutsReducer, { workouts: [] });
-
-//   return (
-//     <WorkoutsContext.Provider value={{ ...state, dispatch }}>
-//       {children}
-//     </WorkoutsContext.Provider>
-//   );
-// };
-
-
 
 import React, { useEffect } from 'react';
 import { useWorkoutsContext } from '../Hooks/useWorkoutsContext';
